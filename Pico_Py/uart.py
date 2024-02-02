3907cb4e4930d810fc6cf5b27051baa185002bb7
weicocogoat <97502018+weicocogoat@users.noreply.github.com>
2024-02-02 02:56:53 +0000

    Update uart.py

--- Pico_Py/uart.py
+++ Pico_Py/uart.py
@@ -2,27 +2,26 @@ from machine import Pin,UART
 import time
 #https://stackoverflow.com/questions/73105957/stm32-memory-dump-and-extracting-secret-key
 #https://forum.micropython.org/viewtopic.php?t=9587/1000
+# https://github.com/newaetech/chipwhisperer/blob/develop/software/chipwhisperer/hardware/naeusb/programmer_stm32fserial.py#L298 (Line 332, 313)
 
 uart = UART(0, baudrate=115200, tx=Pin(12), rx=Pin(13))
-uart.init(bits=8, parity=None, stop=1)
+uart.init(bits=8, parity=0, stop=1) #0 for even parity
 led = Pin("LED", Pin.OUT)
 outp = Pin(3, Pin.OUT)
 
-uart_init = b'\x7f'
+uart_init = b"\x7F"
 read_mem_cmd = b'\x11\xee'
 
 led.value(1)
-outp.value(0)
+outp.value(1)
 
-'''
-time.sleep(2)
-uart.write(hex(127)) #0x7F
-time.sleep(1)
-uart.write(hex(17))   #0x11
-uart.write(hex(238))  #0xEE
-print("sent")
-'''
 
 while True:
+    time.sleep(1)
+    print(f"Sending {uart_init}")
+    uart.flush()
+    time.sleep(0.5)
+    uart.write(uart_init)
+
     if uart.any(): 
-        data = uart.read() 
+        data = uart.read()
