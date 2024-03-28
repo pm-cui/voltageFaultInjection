e9a27c892826194484ab98eee19ff7edd3aa8f3f
weicocogoat <97502018+weicocogoat@users.noreply.github.com>
2024-03-28 08:54:14 +0000

    Add files via upload

--- Week 10 Stuff/Optimizing Glitch/asm code/jump.asm
+++ Week 10 Stuff/Optimizing Glitch/asm code/jump.asm
@@ -2,6 +2,10 @@ jump to reset handler:
     mov r3, #0x08000000     ; -----
     add r3, #0x00000658     ; Base address of reset handler
     mov pc, r3              ; Jump to reset handler
+    nop
+    nop
+    nop
+    nop
 
 
 
@@ -9,3 +13,26 @@ In hexadecimal:
 0x4FF00063
 0x03F5CB63
 0x9F46
+0x00BF
+0x00BF
+
+
+Checksum: 5b
+
+10 bytes
+
+String to be sent:
+31ce
+2000300010
+0d
+4FF0006303F5CB639F4600BF00BF
+56
+
+
+20002ffc
+checksum: f3
+
+
+
+4FF0006300bf00bf
+db
